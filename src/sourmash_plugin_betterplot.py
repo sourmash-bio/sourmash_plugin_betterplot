a567ab9e9bfaf58ac3798e3004e56151a63daab2
C. Titus Brown <titus@idyll.org>
2024-06-13 10:12:20 -0700

    support load/save, but not relabelling

--- src/sourmash_plugin_betterplot.py
+++ src/sourmash_plugin_betterplot.py
@@ -13,6 +13,7 @@ import os
 import csv
 from collections import defaultdict
 from itertools import chain, combinations
+import pickle
 
 import numpy
 import pylab
@@ -934,10 +935,16 @@ class Command_Upset(CommandLinePlugin):
                        help="omit sets below this size or percentage (default: '0%')")
         p.add_argument('--show-percentages', action="store_true",
                        help='show percentages on plot')
+        p.add_argument('--save-intersections-to-file', default=None,
+                       help='save intersections to a file, to avoid expensive recalculations')
+        p.add_argument('--load-intersections-from-file', default=None,
+                       help='load precalculated intersections from a file')
 
-        # add names-from or something @CTB
-        # what's going on with left side?
-        
+#        p.add_argument('--save-names-to-file', default=None,
+#                       help='save set names to a file, for editing & customization')
+#        p.add_argument('--load-names-from-file', default=None,
+#                       help='load set names from a file to customize plots')
+
     def main(self, args):
         super().main(args)
 
@@ -986,27 +993,72 @@ class Command_Upset(CommandLinePlugin):
         names = [ get_name(combo) for combo in pset ]
         notify(f"powerset of distinct combinations: {len(pset)}")
 
-        notify(f"generating intersections...")
-        counts = []
-        nonzero_names = []
-        subtract_me = set()
-        for n, combo in enumerate(pset):
-            if n and n % 10 == 0:
-                notify(f"...{n} of {len(pset)}", end="\r")
+        # CTB: maybe turn the intersection code below into a class?
 
-            combo = list(combo)
-            ss = combo.pop()
-            hashes = set(ss.minhash.hashes) - subtract_me
+        if args.load_intersections_from_file:
+            notify(f"loading intersections from '{args.load_intersections_from_file}'")
+            with open(args.load_intersections_from_file, 'rb') as fp:
+                check_names, nonzero_names, counts = pickle.load(fp)
 
-            while combo and hashes:
+            # confirm!
+            if check_names != names:
+                error("ERROR: saved intersections do not match provided sketches!?")
+                sys.exit(-1)
+        else:
+            notify(f"generating intersections...")
+            counts = []
+            nonzero_names = []
+            subtract_me = set()
+            for n, combo in enumerate(pset):
+                if n and n % 10 == 0:
+                    notify(f"...{n} of {len(pset)}", end="\r")
+
+                combo = list(combo)
                 ss = combo.pop()
-                hashes.intersection_update(ss.minhash.hashes)
+                hashes = set(ss.minhash.hashes) - subtract_me
 
-            if hashes:
-                counts.append(len(hashes) * scaled)
-                nonzero_names.append(names[n])
-                subtract_me.update(hashes)
-        notify(f"\n...done! {len(nonzero_names)} non-empty intersections of {len(names)} total.")
+                while combo and hashes:
+                    ss = combo.pop()
+                    hashes.intersection_update(ss.minhash.hashes)
+
+                if hashes:
+                    counts.append(len(hashes) * scaled)
+                    nonzero_names.append(names[n])
+                    subtract_me.update(hashes)
+            notify(f"\n...done! {len(nonzero_names)} non-empty intersections of {len(names)} total.")
+
+            # maybe decrease memory, but also prevent re/mis-use of these :)
+            del subtract_me
+            del hashes
+#            del names
+
+            if args.save_intersections_to_file:
+                notify(f"saving intersections to '{args.save_intersections_to_file}'")
+                with open(args.save_intersections_to_file, 'wb') as fp:
+                    pickle.dump((names, nonzero_names, counts), fp)
+
+#        if args.save_names_to_file:
+#            with open(args.save_names_to_file, 'w', newline='') as fp:
+#                w = csv.writer(fp)
+#                w.writerow(['sort_order', 'name'])
+#                for n, name in enumerate(names):
+#                    w.writerow([n, name])
+#            notify(f"saved {len(names)} names to '{args.save_names_to_file}'")
+
+#        if args.load_names_from_file:
+#            with open(args.load_names_from_file, 'r', newline='') as fp:
+#                r = csv.DictReader(fp)
+#                rows = list(r)
+#                if 'sort_order' not in rows[0].keys():
+#                    error("'sort_order' must be a column in names file '{args.load_names_from_file}'")
+#                if 'name' not in rows[0].keys():
+#                    error("'name' must be a column in names file '{args.load_names_from_file}'")
+#
+#                rows.sort(key=lambda x: int(x["sort_order"]))
+#                names = [ row["name"] for row in rows ]
+#            notify("loaded {len(names)} names from '{args.load_names_from_file}'")
+
+        ## now! calculate actual data for upsetplot...
 
         data = upsetplot.from_memberships(nonzero_names, counts)
 
